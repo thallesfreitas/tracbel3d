571b20e2974a71676c61a40f18f70fb0f4c80880
Thalles Freitas <thallesfreitas@yahoo.com.br>
2024-02-07 19:27:22 -0300

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
         footer: "url('images/footer/bg-footer.png')",
         contact: "url('/images/form/banner-form.webp')",
         contact2: "url('/projetos/tracbel/lp/images/form/banner-form.webp')",
-        // contactIa: "url('images/fundo-ia.jpg')",
+        // contactIa: "url('images/fundo-ia.jpg')2",
       },
       fontFamily: {
         abeezee: ["ABeeZee-Regular", "sans-serif"],
